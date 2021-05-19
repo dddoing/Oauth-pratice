a96151cdd135b3bca24fb84f6a5fb2ea8375d69b
유도건 <dgyoo@nextree.io>
2021-05-19 16:24:40 +0900

    pratice

--- src/main/java/com/test/demo/controller/common/Oauth2Controller.java
+++ src/main/java/com/test/demo/controller/common/Oauth2Controller.java
@@ -3,11 +3,16 @@ package com.test.demo.controller.common;
 import com.google.gson.Gson;
 import com.test.demo.model.oauth2.OAuthToken;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +25,26 @@ public class Oauth2Controller {
     @GetMapping("/callback")
     public OAuthToken callbackSocial(@RequestParam String code) {
         //
+
+        String credentials = "testClientId:testSecret";
+        String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Authorization","Basic "+encodedCredentials);
+
+        MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+        params.add("code",code);
+        params.add("grant_type","authorization_code");
+        params.add("redirect_uri","http://localhost:8081/oauth2/callback");
+
+        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(params,headers);
+        System.out.println(request);
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8081/oauth/token",request,String.class);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return gson.fromJson(response.getBody(),OAuthToken.class);
+        }
         return null;
     }
 }
